2d9674fa1731450da7ec9ec9123892f533ee06b2
Alexander Vas <buhu_iiux@mail.ru>
2022-02-24 00:40:04 +0300

    Start Interface

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ company = Company()
 company.add_laboratories(lab)
 company.add_customers(customer)
 
-interface(company.get_labs_list(), company.get_cust_list(), ["sadas", "sdsad", "fdfd"], ["ewqrt", "ety", "dfd", "fd"],
-          ["dfdsfsd", "dfsdf", "gfg"])
+# interface(company.get_labs_list(), company.get_cust_list(), ["sadas", "sdsad", "fdfd"], ["ewqrt", "ety", "dfd", "fd"],
+#           ["dfdsfsd", "dfsdf", "gfg"])
 
 
 
-# generate_protocol(lab, customer, "20 февраля 2022 г.", "14.02.2022 г.")
+generate_protocol(lab, customer, "20 февраля 2022 г.", "14.02.2022 г.")
 
